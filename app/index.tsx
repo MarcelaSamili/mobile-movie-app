a70e856118ca1f1a474952869c72389153533478
Marcela Samili <85258527+MarcelaSamili@users.noreply.github.com>
2025-03-12 15:58:25 -0300

    Configurations to style
    The style will be make to tailwind so need thise configurations.

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View className="bg-slate-600 flex-1 justify-center items-center ">
+      <Text className="text-xl text-white">Welcome to the movie</Text>
+    </View>
+  );
+}
