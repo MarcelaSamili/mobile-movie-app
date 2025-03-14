7c3436644d0b59cf1aa0cbce16855a1fab96108d
Marcela Samili <85258527+MarcelaSamili@users.noreply.github.com>
2025-03-14 16:44:07 -0300

    Maked the Movies scroll
    Using the api, we make the movies cards.

--- services/api.ts
+++ services/api.ts
@@ -1,43 +1,29 @@
 export const TMDB_CONFIG = {
-  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_URL: 'https://api.themoviedb.org/3',
   API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
   headers: {
     accept: 'application/json',
-    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_BEARER}`,
   },
 };
-
 export const fetchMovies = async ({ query }: { query: string }) => {
   const endpoint = query
-    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
-    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(
+        query
+      )}&api_key=${TMDB_CONFIG.API_KEY}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${TMDB_CONFIG.API_KEY}`;
 
   const response = await fetch(endpoint, {
     method: 'GET',
-    headers: TMDB_CONFIG.headers,
+    headers: {
+      accept: 'application/json',
+    },
   });
   if (!response.ok) {
     // @ts-ignore
-    throw new Error('Failed to fetch movies' /*response.statusText*/);
+    throw new Error('Failed to fetch movies' /*response.response*/);
   }
   const data = await response.json();
 
   return data.results;
 };
-
-// /discover/movie
-/*const url =
-  'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNmU0NjcxNjFiMjBhMDhhOGVmYTFkMDk0MDRmZGMyNiIsIm5iZiI6MTc0MTg4NzE4MC44NDcwMDAxLCJzdWIiOiI2N2QzMTZjYzQzNGM5OGM4ZWM4MWNiZGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.yj9bTFEUefkvc21O-vyO-gh0XPpssMwxQbOdCtaksyU',
-  },
-};
-
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error(err));*/
